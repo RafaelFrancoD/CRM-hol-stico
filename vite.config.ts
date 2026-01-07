786267e2f62c7a310f891c536fe5190c188922db
Seu Nome <rafael.personal83@hotmail.com>
2026-01-06 21:31:20 -0300

    Ajustar config para usar VITE_API_URL

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    // Usa process.env para acessar variáveis de ambiente passadas pelo Docker
-    const apiTarget = process.env.VITE_API_TARGET || 'http://localhost:4000';
+    // Usa process.env para acessar variáveis de ambiente passadas pelo Docker ou Vercel
+    const apiTarget = process.env.VITE_API_URL || process.env.VITE_API_TARGET || 'http://localhost:4000';
 
     return {
       server: {
